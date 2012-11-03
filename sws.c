f624f2bf9cfededd79d17326f8f8485d25d688f6
boyu2011 <boyu2011@gmail.com>
2012-11-03 16:31:46 -0400

    now sws can process static files

--- sws.c
+++ sws.c
@@ -12,17 +12,145 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <strings.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define DEBUG
+#define MAX_LEN 1024
+
+/*
+    read first line ( request line ) from http request
+*/
+
+ssize_t readline ( int fd, void * userbuf, size_t maxlen )
+{
+    int n, read_cnt;
+    char c;
+    char * bufp = userbuf;
+
+    for ( n = 1; n < maxlen; n++ )
+    {
+        if ( (read_cnt = read ( fd, &c, 1 )) == 1 )
+        {
+            *bufp++ = c;
+            if ( c == '\n' )
+            {
+                break;
+            }
+        }
+        else if ( read_cnt == 0 )
+        {
+            if ( n == 1 )
+                return 0;   /* EndOfFile, no data read */
+            else
+                break;      /* EndOfFile, data was read */
+        }
+        else
+        {
+            return -1;      /* error */
+        }
+    }
+    *bufp = 0;
+    return n;
+}
+
+/*
+    send error message to client side
+*/
+void client_error ( int fd, char * error_cause, char * error_number,
+                    char * error_reason_phrase )
+{
+    char buf [MAX_LEN];
+    char body [MAX_LEN];
+
+    /* build the HTTP response body */
+    sprintf ( body, "<html><title>Error</title>\r\n" );
+    sprintf ( body, "%s%s: %s\r\n", body, error_number, error_reason_phrase );
+    sprintf ( body, "%s<br/>%s\r\n", body, error_cause );
+    sprintf ( body, "%s<hr>Simple Web Server\r\n", body );
+
+    /* write the HTTP response */
+    sprintf ( buf, "HTTP/1.0 %s %s\r\n", error_number, error_reason_phrase );
+    if ( write ( fd, buf, strlen(buf) ) != strlen (buf) )
+    {
+        perror ( "write1" );
+    }
+    sprintf ( buf, "Content-type: text/html\r\n" );
+    if ( write ( fd, buf, strlen(buf) ) != strlen (buf) )
+    {
+        perror ( "write2" );
+    }
+    sprintf ( buf, "Content-length: %d\r\n\r\n", (int)strlen(body) );
+    if ( write ( fd, buf, strlen(buf) ) != strlen(buf) )
+    {
+        perror ( "write3" );
+    }
+    if ( write ( fd, body, strlen(body) ) )
+    {
+        perror ( "write4" );
+    }
+}
+
+void parse_uri ( char * uri, char * filename )
+{
+    /* remove the first char '/' */
+    strcpy ( filename, &uri[1] );
+}
+
+
+void handle_regular_file ( int fd, char * filename )
+{
+    char buf [MAX_LEN];
+    char body [MAX_LEN];
+    int file_fd;
+
+    /* build body */
+    file_fd = open ( filename, O_RDONLY );
+    if ( file_fd < 0 )
+    {
+        perror ( "open" );
+        exit(1);
+    }
+    if ( read ( file_fd, body, sizeof(body) ) < 0 )
+    {
+        perror ( "read" );
+        exit(1);
+    }
+
+    /* write http response */
+    sprintf ( buf, "HTTP/1.0 200 OK\r\n" );
+    sprintf ( buf, "%sServer: Simple Web Server\r\n", buf );
+    sprintf ( buf, "%sContent-length: 20\r\n", buf );
+    sprintf ( buf, "%sContent-type: text/html\r\n\r\n", buf );
+    /*
+    sprintf ( buf, "%s<html>this is an amazing time!!!</html>\r\n", buf );
+    */
+    sprintf ( buf, "%s%s", buf, body );
+    
+    if ( write ( fd, buf, strlen(buf) ) != strlen(buf) )
+    {
+        perror ( "write" );
+    }
+    else
+    {
+        printf ( "file has been sent!!\n" );
+    }
+}
 
 int main ( int argc, char ** argv )
 {
     int socket_fd;
+    int conn_socket_fd;
     struct sockaddr_in server;
     int server_len;
-    int msg_socket;
-    char buf[1024];
+    char buf[MAX_LEN];
+    char method [MAX_LEN];
+    char request_uri [MAX_LEN];
+    char http_version [MAX_LEN];
     int status;
     pid_t pid;
-    int read_cnt;
+    char file_name[MAX_LEN];
+    struct stat sbuf;
 
     /* 
         parse command line options 
@@ -77,18 +205,15 @@ int main ( int argc, char ** argv )
         run as a daemon, loop forever
     */
 
-    /* .... */
-
 
     while (1)
     {
-
         /*
             accept connection
         */
 
-        msg_socket = accept ( socket_fd, 0, 0 );
-        if ( msg_socket == -1 )
+        conn_socket_fd = accept ( socket_fd, 0, 0 );
+        if ( conn_socket_fd == -1 )
         {
             perror ( "accept" );
             continue;
@@ -112,6 +237,13 @@ int main ( int argc, char ** argv )
         {
             /* reading request from socket */
 
+            bzero ( buf, sizeof(buf) );
+            readline ( conn_socket_fd, buf, sizeof(buf) );
+#ifdef DEBUG
+            printf ( "[DEBUG INFO] Request-Line : %s\n", buf );
+#endif
+
+/*
             do {
                 bzero ( buf, sizeof(buf) );
                 if ( ( read_cnt = read ( msg_socket, buf, 1024 ) ) < 0 )
@@ -129,24 +261,62 @@ int main ( int argc, char ** argv )
                 }
         
             }while ( read_cnt != 0 );
+            printf ( "out of while ( read_cnt!=0 )\n" );
+*/
 
-            /* parsing request */
+            /* 
+                parsing request
+            */
+            
+            sscanf ( buf, "%s %s %s", method, request_uri, http_version );
 
-                /* valid syntax? */
+            /* valid syntax? */
 
-                /* type of request */
+            /* type of request */
+            if ( strcmp ( method, "GET" ) )
+            {
+                client_error ( conn_socket_fd,
+                               "Simple Web Server does not implement this method",
+                               "501",
+                               "Not Implemented" );
 
-                /* determine pathname ( ~ translation /
-                   trslate relative into absolute pathname ) */
+                if ( close ( conn_socket_fd ) != 0 )
+                {
+                    perror ( "close socket" );
+                    exit(1);
+                }
+                continue;
+            }
 
-            /* generate server status response */
+            /* determine pathname ( ~ translation /
+               trslate relative into absolute pathname ) */
+            parse_uri ( request_uri, file_name );
+            if ( stat ( file_name, &sbuf ) < 0 )
+            {
+                client_error ( conn_socket_fd,
+                               "Simple Web Server can't find this file",
+                               "404",
+                               "Not Found" );
+
+                if ( close ( conn_socket_fd ) != 0 )
+                {
+                    perror ( "close socket" );
+                    exit(1);
+                }
+                continue;
+            }
+
+            /* 
+                generate server status response
+            */
 
             /* handle request */
 
                 /* handle regular file request */
-
         
                     /* stat(2), open(2), read(2), write(2), close(2) */
+
+                    handle_regular_file ( conn_socket_fd, file_name ); 
 
                     /* terminate connection, exit child handler */
 
@@ -160,29 +330,39 @@ int main ( int argc, char ** argv )
 
         }
 
-        /* parent !!!need more considerable!!! */
+        /* parent !!!need more consideration!!! */
         if ( pid != 0 )
         {
             if ( waitpid ( pid, &status, 0 ) != pid )
             {
-                perror ( "wait.." );
+                perror ( "wait" );
                 exit(1);
             }
         }
 
         /* close socket */
-        if ( close ( msg_socket ) != 0 )
+        if ( close ( conn_socket_fd ) != 0 )
         {
             perror ( "close socket" );
             exit(1);
         }
-    }
+
+    } /* end of while(1) */
 
     /*
         upon SIGHUP re-read configuration, restart
     */
 
 
+    /* 
+        close accept socket fd
+    */
+    
+    if ( close ( socket_fd ) != 0 )
+    {
+        perror ( "close socket" );
+        exit(1);
+    }
 
     exit(0);
 }
